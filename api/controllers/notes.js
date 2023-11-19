489d34525a597333f77c671406ddeb2520435f36
Kayo Silva <Kayohenriquesilva19@gmail.com>
2023-11-19 13:26:21 -0300

    Config api

---
+++ api/controllers/notes.js
@@ -0,0 +1,17 @@
+import { db } from '../db.js'
+
+export const getNotes = (req, res) => {
+  return res.status(200).json({ok: 'AQUI'}) 
+}
+
+export const addNote = () => {
+  
+}
+
+export const updateNote = () => {
+  
+}
+
+export const deleteNote = () => {
+  
+} 
---
+++ api/routes/notes.js
@@ -0,0 +1,11 @@
+import express from "express";
+import {getNotes, addNote, updateNote, deleteNote} from '../controllers/notes.js'
+
+const router = express.Router();
+
+router.get('/notes', getNotes) 
+router.post('/notes', addNote) 
+router.put('/notes/:id', updateNote) 
+router.delete('/notes/:id', deleteNote) 
+
+export default router
